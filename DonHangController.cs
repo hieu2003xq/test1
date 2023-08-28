b9ca5cbacfbca276548c8711353bdae1361aa542
hieu <letrunghieuxq@gmail.com>
2023-08-28 15:38:51 +0700

    #4- gioHanf,donhang

---
+++ DonHangController.cs
@@ -0,0 +1,152 @@
+﻿using appweb.help;
+using appweb.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.Migrations;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace appweb.Controllers
+{
+    public class DonHangController : Controller
+    {
+        cart  cart =new cart();
+        
+        // GET: DonHang
+        [HttpGet]
+        public ActionResult Index(int id)
+        {
+          
+            if (Session["gmail"] != null)
+            {
+                var key = Session["gmail"].ToString();
+                qlBhEntities db = new qlBhEntities();
+                List<gioHang>a=memory_cache.getValue(key) as List<gioHang>;
+
+                if (a != null)
+                {
+                    gioHang sp = a.Find(m => m.maSP == id);
+                    donHang b=db.donHang.Where(m=>m.maSP==sp.maSP).FirstOrDefault();
+                    if (b != null)
+                    {
+                        b.soLuong++;
+                        b.thanhtien = sp.giaBan * b.soLuong;
+                        b.tongTien = b.thanhtien;
+                        b.Gmail = key;
+                        sp.soLuong--;
+                        if(sp.soLuong == 0)
+                        {
+                            a.Remove(sp);
+                        }
+                    }
+                    else
+                    {
+                        b=new donHang();
+                        b.Gmail = key;
+                        b.soLuong = 1;
+                        b.maSP=sp.maSP;
+                        b.thanhtien = sp.giaBan * b.soLuong;
+                        b.tongTien = b.thanhtien;
+                        sp.soLuong--;
+                        if (sp.soLuong == 0)
+                        {
+                            a.Remove(sp);
+                        }
+
+                    }
+                    db.donHang.AddOrUpdate(b);
+                    db.SaveChanges();
+
+                }
+                
+                return RedirectToAction("GioHang","gioHang");
+            }
+            return RedirectToAction("GioHang", "gioHang");
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Index(int id,string tenkh,string sdt,string diachi)
+        {
+            qlBhEntities db=new qlBhEntities();
+            List<gioHang> a = cart.laygioHang();
+            if (a != null)
+            {
+                DonHangRieng b = new DonHangRieng();
+                gioHang sp = a.Find(m => m.maSP == id);
+                if (b.maSP != sp.maSP)
+                {
+                    b.maSP = sp.maSP;
+                    b.tenKH = tenkh;
+                    b.SDT = sdt;
+                    b.diaChi = diachi;
+                    b.soLuong = 1;
+                    b.thanhTien = sp.giaBan * b.soLuong;
+                    b.tongTien = b.thanhTien;
+                    sp.soLuong--;
+                    if (sp.soLuong == 0)
+                    {
+                        a.Remove(sp);
+                    }
+                    db.DonHangRieng.Add(b);
+
+
+                }
+                else
+                {
+                    b.soLuong++;
+                }
+                db.SaveChanges();
+            }
+
+            return RedirectToAction("GioHang", "gioHang");
+        }
+        [HttpGet]
+        public ActionResult thongtinDon(int id) 
+        {
+            if (Session["gmail"] == null)
+            {
+                donHang c=new donHang();
+                c.soLuong = 1;
+                List<gioHang> a = cart.laygioHang();
+                gioHang b = a.Find(m => m.maSP == id);
+                ViewBag.thanhtien = b.giaBan * c.soLuong;
+                return View(b);
+            }
+            else
+            {
+                donHang c = new donHang();
+                c.soLuong = 1;
+                List<gioHang> a = cart.laygioHang(Session["gmail"].ToString()) ;
+                gioHang b = a.Find(m => m.maSP == id);
+                ViewBag.thanhtien = b.giaBan * c.soLuong;
+                return View(b);
+            }
+       
+        }
+        public ActionResult DanhSachDon()
+        {
+            qlBhEntities db=new qlBhEntities();
+            if (Session["gmail"] != null)
+            {
+                var key = Session["gmail"].ToString();
+                List<donHang> a = db.donHang.Where(m => m.Gmail == key).ToList();
+                if(a==null)
+                {
+                    a=new List<donHang>();
+                }
+
+               return View(a);
+            }
+            else
+            {
+                List<DonHangRieng> a=new List<DonHangRieng>();
+            }
+            return View();
+        }
+        
+        
+
+    }
+}
---
+++ gioHangController.cs
@@ -0,0 +1,188 @@
+﻿using appweb.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using appweb.Controllers;
+using appweb.help;
+using System.Web.Services.Protocols;
+using System.Security.Cryptography.X509Certificates;
+using System.Security.Cryptography.Pkcs;
+using System.Web.Razor.Generator;
+
+namespace appweb.Controllers
+{
+    public class gioHangController : Controller
+    {
+      qlBhEntities db=new qlBhEntities();   
+
+        // GET: gioHang
+        cart b=new cart();
+
+        [HttpGet]
+        public ActionResult themGioHang(int id)
+        {
+            qlBhEntities db = new qlBhEntities();
+            if (Session["gmail"] != null)
+            {
+                var key = Session["gmail"].ToString();
+                if (key != null)
+                {
+                    List<gioHang> a = b.laygioHang(key);
+                    gioHang sp = a.Find(m => m.maSP == id);
+                        if(sp != null)
+                        {
+                         sp.soLuong++;
+                      }
+                      else
+                     {
+                        sp = new gioHang(id);
+                        a.Add(sp);
+
+                    }memory_cache.add(key,a,DateTimeOffset.Now.AddDays(1));
+                    return RedirectToAction("GioHang");
+                }
+                return RedirectToAction("GioHang");
+
+            }
+            else
+            {
+                List<gioHang> a = b.laygioHang();
+                gioHang sp = a.Find(m => m.maSP == id);
+                if (sp != null)
+                {
+                    sp.soLuong++;
+                }
+                else
+                {
+                    sp = new gioHang(id);
+                    a.Add(sp);
+                }
+                return RedirectToAction("GioHang");
+            }
+
+
+        }
+        [HttpGet]
+        public ActionResult GioHang()
+        {
+            
+            if (Session["gmail"] != null)
+            {
+                var key = Session["gmail"].ToString();
+
+                if (key != null)
+                {
+
+                    List<gioHang> a = b.laygioHang(key);
+                    ViewBag.tongtien =  b.tongTien(key);
+                    ViewBag.tongSL = b.tongSL(key);
+                    if (a == null)
+                    {
+                        a = new List<gioHang>();
+                        memory_cache.add(key, a, DateTimeOffset.Now.AddDays(1));
+                    }
+                    else
+                    {
+                        return View(a);
+                    }
+
+                    return View(a);
+                }
+
+            }
+            else
+            {
+                List<gioHang> a = b.laygioHang();
+                ViewBag.gioHang = b.laygioHang();
+                ViewBag.tongtien = b.tongTien();
+                ViewBag.tongSL = b.tongSL();
+                if (a.Count() == 0)
+                {
+                    RedirectToAction("Index", "Home");
+                }
+                else
+                {
+                    return View(a);
+                }
+            }
+            
+            return View(); 
+            
+
+
+        }
+        [HttpGet]
+        public ActionResult xoaGioHang(int id)
+        {
+            
+            qlBhEntities db = new qlBhEntities();
+            if (Session["gmail"] != null)
+            {
+                var key = Session["gmail"].ToString();
+                List<gioHang>a=memory_cache.getValue(key)as List<gioHang>;
+                if(a != null)
+                {
+                    gioHang sp = a.Find(m => m.maSP == id);
+                    if(sp != null)
+                    {
+                        sp.soLuong--;
+                        if(sp.soLuong == 0)
+                        {
+                            a.Remove(sp);
+                        }
+                    }
+                    memory_cache.add(key, a, DateTimeOffset.Now.AddDays(1));
+                    return RedirectToAction("GioHang");
+
+                }
+                else
+                {
+                    memory_cache.delete(key);
+                    return RedirectToAction("GioHang");
+                }
+
+            }
+            else
+            {
+                List<gioHang> a = b.laygioHang();
+                if (a != null)
+                {
+                    gioHang sp = a.Find(m => m.maSP == id);
+                    if (sp != null)
+                    {
+                        sp.soLuong--;
+                        if (sp.soLuong == 0)
+                        {
+                            a.Remove(sp);
+                        }
+                    }
+                    return RedirectToAction("GioHang");
+                }
+            }
+            return RedirectToAction("GioHang");
+
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult TimKiem(string ten)
+        {
+          
+            qlBhEntities db = new qlBhEntities();
+            Session["ten"] = ten;
+            List<sanPham> sp = db.sanPham.Where(m => m.tenSP.ToLower().Contains(ten.ToLower()) == true).ToList();
+            if (sp.Count > 0)
+            {
+                return View(sp);
+            }
+            else
+            {
+                TempData["kTimThay"] = "khong tim thay san pham";
+               
+                return RedirectToAction("GioHang");
+            }
+
+        }
+    }             
+    }
